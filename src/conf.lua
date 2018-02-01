24c6ac3ee4e058de84e62deb66bcbaffa82216b6
[MEADOW_DEV] <bendixon50@gmail.com>
2018-02-01 14:21:01 +0000

    :wrench: Config file fix

--- src/conf.lua
+++ src/conf.lua
@@ -6,8 +6,8 @@ function love.conf(c)
   c.console = true
 
   local window = c.screen or c.window -- love 0.9 renamed "screen" to "window"
-  window.width = 720/2 -- Set window width to 720/2(360)
-  window.height = 1280/2 -- Set window height to 1280/2(640)
+  window.width = 1280 -- Set window width to 720/2(360)
+  window.height = 720 -- Set window height to 1280/2(640)
   window.vsync = false -- Disable V-Sync
   window.highdpi = true -- Enable High DPI mode
   window.msaa = 4 -- Sets msaa sampling to 4
