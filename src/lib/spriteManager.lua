b25c5b0d7b2c542916cf100d2b8ebc94a0e0196a
[MEADOW_DEV] <bendixon50@gmail.com>
2018-02-07 14:47:34 +0000

    :bento: Added UI assets

--- src/lib/spriteManager.lua
+++ src/lib/spriteManager.lua
@@ -2,15 +2,26 @@ log = require('lib/plugins/Log')
 
 spriteManager = {}
 spriteManager.sprites = {}
+spriteManager.sprites.entities = {}
+spriteManager.sprites.weapons = {}
+spriteManager.sprites.ui = {}
 
 -- Load sprites
 function spriteManager.Load()
   log.info("[INIT]: Loading sprites")
 
-  spriteManager.sprites.playerSpritesheet = love.graphics.newImage("assets/sprites/playerSpritesheet.png")
-  spriteManager.sprites.shotgunSpritesheet = love.graphics.newImage("assets/sprites/playerSpritesheet.png")
-  spriteManager.sprites.pistolSpritesheet = love.graphics.newImage("assets/sprites/playerSpritesheet.png")
-  spriteManager.sprites.zombieSpritesheet = love.graphics.newImage("assets/sprites/playerSpritesheet.png")
+  -- Entities
+  spriteManager.sprites.entities.playerSpritesheet = love.graphics.newImage("assets/sprites/entities/playerSpritesheet.png")
+  spriteManager.sprites.entities.zombieSpritesheet = love.graphics.newImage("assets/sprites/entities/zombieSpritesheet.png")
+
+  -- Weapons
+  spriteManager.sprites.weapons.shotgunSpritesheet = love.graphics.newImage("assets/sprites/weapons/spritesheet_shotgun.png")
+  spriteManager.sprites.weapons.pistolSpritesheet = love.graphics.newImage("assets/sprites/weapons/spritesheet_pistol.png")
+
+  -- UI spritesheets
+  spriteManager.sprites.ui.red = love.graphics.newImage("assets/sprites/ui/redSheet.png")
+  spriteManager.sprites.ui.green = love.graphics.newImage("assets/sprites/ui/greenSheet.png")
+  spriteManager.sprites.ui.blue = love.graphics.newImage("assets/sprites/ui/blueSheet.png")
 end
 
 function spriteManager.Update(dt)
