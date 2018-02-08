a0732b5200807176a33a748b5da8548578a26afd
[MEADOW_DEV] <bendixon50@gmail.com>
2018-02-08 14:31:06 +0000

    :tada: Added initial fullscreen toggle(WIP)

--- src/main.lua
+++ src/main.lua
@@ -6,12 +6,14 @@ debugger = require('lib/debugger')
 spriteManager = require('lib/spriteManager')
 audioManager = require('lib/audioManager')
 
+-- Config options
+config = require('conf')
+
 -- Initialize debugger math helper
 debugger.fpsMath()
 
 -- Love2D Initialization
 function love.load()
-
   -- Initialize Shack to game window dimensions
   shack:setDimensions(love.graphics.getDimensions())
 
@@ -27,6 +29,11 @@ end
 function love.keypressed(key)
   -- Debugger graph toggle
   debugger.keypressed(key, "f12")
+
+  -- Toggle fullscreen
+  if (key == "f11") then
+    config.toggleFullscreen()
+  end
 
   if (key == "f") then
     shack:setShake(20)
